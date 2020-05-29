1a935d21e3a69e01055dad82f75150852fa5d1a3
kzhub <55772750+kzhub@users.noreply.github.com>
2020-05-29 13:45:06 +0900

    雛形アップ

---
+++ script.js
@@ -0,0 +1,28 @@
+function pen(){
+  let modal= document.getElementById("modal");
+  modal.style.display = 'block';
+  let mask= document.getElementById("mask");
+  mask.style.display = 'block';
+
+}
+
+function pen2(){
+  let modal= document.getElementById("modal");
+  modal.style.display = 'none';
+  let mask= document.getElementById("mask");
+  mask.style.display = 'none';
+
+}
+
+function pen3(){
+  let modal= document.getElementById("modal");
+  modal.style.display = 'none';
+  let mask= document.getElementById("mask");
+  mask.style.display = 'none';
+
+}
+
+function hide(){
+  let ab= document.getElementById('kesu');
+  ab.style.display = 'none';
+}
